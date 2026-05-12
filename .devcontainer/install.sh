93eebeefaabed99d6faee312c8bcf325778e6456
bitterbi3cuit <evilmamaliz@gmail.com>
2026-05-12 23:12:16 +0330

    Update install.sh

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "downloading xray"
-wget -O ${PWD}/xray.zip https://github.com/XTLS/Xray-core/releases/download/v26.3.27/Xray-linux-64.zip
+wget -O ${PWD}/xray.zip https://github.com/XTLS/Xray-core/releases/download/v26.5.9/Xray-linux-64.zip
 
 echo "installing"
 unzip xray.zip && chmod +x xray
